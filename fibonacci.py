2fff9813803c588e328939ba8ab05cda0ef96eef
Rea20 <97180540+Rea20@users.noreply.github.com>
2022-02-07 22:40:57 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  4 22:19:24 2022
+
+@author: User
+
+def fib(10):
+    a=0
+    b=1
+    if n==1:
+        print('0')
+    elif n==2:
+        print('0','1')
+    else:
+        print("fibonacci series using Iterative approach")
+        print('0'',a,b,end=')
+        for i in range (n-3):
+            total= a+b
+            b=a
+            a=total 
+            print(total,end='n')
